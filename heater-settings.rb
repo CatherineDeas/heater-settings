423a68ebb58269afcb8b68bd3ff1c738f31a4fad
Catherine Deas <catherine.deas@madetech.com>
2022-05-26 21:04:11 +0100

    Update heater-settings to handle temperatures up to 14C

--- heater-settings.rb
+++ heater-settings.rb
@@ -1,4 +1,16 @@
 def find_setting(temperature)
-    return 4    
+    if temperature == 0
+        return 4    
+    elsif temperature == 3
+        return 3.5   
+    elsif temperature == 6
+        return 3
+    elsif temperature == 9
+        return 2.5
+    elsif temperature == 12
+        return 2
+    else
+        return 1.5
+    end
 end
 
--- spec/heater-settings_spec.rb
+++ spec/heater-settings_spec.rb
@@ -10,5 +10,56 @@ describe 'the required setting when given a temperature value' do
         #Assert
         expect(result).to eq(setting)
     end
+
+    it 'given a temperature of 3C, returns a setting of 3.5' do
+        #Arrange
+        temperature = 3
+        setting = 3.5
+        #Act
+        result = find_setting(temperature)
+        #Assert
+        expect(result).to eq(setting)
+    end
+
+    it 'given a temperature of 6C, returns a setting of 3' do
+        #Arrange
+        temperature = 6
+        setting = 3
+        #Act
+        result = find_setting(temperature)
+        #Assert
+        expect(result).to eq(setting)
+    end
+
+    it 'given a temperature of 9C, returns a setting of 2.5' do
+        #Arrange
+        temperature = 9
+        setting = 2.5
+        #Act
+        result = find_setting(temperature)
+        #Assert
+        expect(result).to eq(setting)
+    end
+
+    it 'given a temperature of 12C, returns a setting of 2' do
+        #Arrange
+        temperature = 12
+        setting = 2
+        #Act
+        result = find_setting(temperature)
+        #Assert
+        expect(result).to eq(setting)
+    end
+
+    it 'given a temperature of 14C, returns a setting of 1.5' do
+        #Arrange
+        temperature = 14
+        setting = 1.5
+        #Act
+        result = find_setting(temperature)
+        #Assert
+        expect(result).to eq(setting)
+    end
+
 end
  
