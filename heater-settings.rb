2a93a66f261015ac04eafea7063d74e021b6ad51
CatherineDeas <catherine.deas@madetech.com>
2022-06-23 21:23:06 +0100

    Update heater settings to handle ranges

--- heater-settings.rb
+++ heater-settings.rb
@@ -1,17 +1,17 @@
 def find_setting(temperature)
-    if temperature < 0
+    if temperature <= 0
         return 4.5 
-    elsif temperature == 0
-        return 4    
-    elsif temperature == 3
-        return 3.5   
-    elsif temperature == 6
+    elsif temperature > 0 && temperature <= 3
+        return 4 
+    elsif temperature > 3 && temperature <= 6
+        return 3.5
+    elsif temperature > 6 && temperature <= 9
         return 3
-    elsif temperature == 9
+    elsif temperature > 9 && temperature <= 12
         return 2.5
-    elsif temperature == 12
+    elsif temperature > 12 && temperature <= 14
         return 2
-    elsif temperature == 14
+    elsif temperature > 14 && temperature < 16
         return 1.5
     elsif temperature == 16
         return 1
